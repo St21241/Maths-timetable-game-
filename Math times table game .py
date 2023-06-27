52d6587d3b6991d8224ccbe672bb5bfdb97b95e0
Navjot <st21241@ormiston.school.nz>
2023-06-27 13:59:06 +1200

    test 1

--- Math times table game .py
+++ Math times table game .py
@@ -1,8 +1,9 @@
 from tkinter import *
+import random
 
 root = Tk()
 # Creating a Label Widget 
-myLabel = Label(root, text="Hello World")
+myLabel = Label(root, text=str(random.random()))
 # shoving it onto the screen
 myLabel.pack()
 
